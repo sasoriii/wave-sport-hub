55e39b2ec03ffa2d56fa8d32f3ee2826818144e6
sasoriii <jeremy.gabriel@viacesi.fr>
2024-12-12 15:45:06 +0100

    refactor: update image URLs and nanoid version in Index component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Index from "./pages/Index";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-// const App = () => (
-//   <QueryClientProvider client={queryClient}>
-//     <TooltipProvider>
-//       <Toaster />
-//       <Sonner />
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Index />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </TooltipProvider>
-//   </QueryClientProvider>
-// );
-
-// export default App;
-import React from "react";
-import BookingForm from "./components/BookingForm";
-import GoogleCalendar from "./components/GoogleCalendar";
-
-const App = () => {
-  return (
-    <div>
-      <h1>Wave Sport Hub</h1>
-      <GoogleCalendar />
-      <BookingForm />
-    </div>
-  );
-};
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
+// import React from "react";
+// import BookingForm from "./components/BookingForm";
+// import GoogleCalendar from "./components/GoogleCalendar";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Wave Sport Hub</h1>
+//       <GoogleCalendar />
+//       <BookingForm />
+//     </div>
+//   );
+// };
+
+// export default App;
